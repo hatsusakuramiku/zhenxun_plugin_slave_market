0efcd20ebeefc15eba981780a2d265680d1c25d1
hatsusakuramiku <hcj13794190602@gmail.com>
2023-06-24 15:37:11 +0800

    Update __init__.py
    为规范劳动市场（捞油水），群友所执行的每一笔交易与打工真寻都会收税，税率请发送"市场税率"查看

--- zhenxun_plugin_slave_market/__init__.py
+++ zhenxun_plugin_slave_market/__init__.py
@@ -30,10 +30,12 @@ usage:
     发送购买群友+@群友 可买下群友帮你打工
     发送我的群友查看 我当前已经购买的群友，
     发送一键打工 可让你的所有群友为你打工（没有群友就自己打工）
+    发送市场税率 查看真寻的税率
+    为规范劳动市场（捞油水），群友所执行的每一笔交易与打工真寻都会收税，税率请发送"市场税率"查看
         
 """.strip()
 __plugin_des__ = "群友交易市场"
-__plugin_cmd__ = ["群友市场", "购买群友"]
+__plugin_cmd__ = ["群友市场", "购买群友","市场税率"]
 __plugin_type__ = ("群内小游戏",)
 __plugin_settings__ = {
     "level": 5,
@@ -118,37 +120,41 @@ async def _(bot:Bot, event: GroupMessageEvent):
     if not user:
         #第一次被买，钱给黑奴
         if await BagUser.get_gold(user_id,group_id)<100:
-            await buyuser.finish(f"你买不起！需要{auser.body_price}金币", at_sender=True)
+            await buyuser.finish(f"你买不起！需要{auser.body_price + tax(auser.body_price)}金币", at_sender=True)
             return
         m = await UsersInfo.add_user(user_id,group_id,at[0])
-        await BagUser.add_gold(at[0],group_id,m)
-        await BagUser.spend_gold(user_id,group_id,m)
+        a_tax_s=m + tax(m)
+        a_tax_a=m - tax(m)
+        await BagUser.add_gold(at[0],group_id,a_tax_a)
+        await BagUser.spend_gold(user_id,group_id,a_tax_s)
         a = await BagUser.get_gold(at[0],group_id)
         u = await BagUser.get_gold(user_id,group_id)
-        msg=f"成功购买了{user_name}！花费了{m}金币，还剩{u}金币\n{user_name}获得了{m}金币，现在拥有{a}金币，身价上涨{m}->{m+20}"
+        msg=f"成功购买了{user_name}！花费了{a_tax_s}金币，还剩{u}金币\n{user_name}获得了{a_tax_a}金币，现在拥有{a}金币，身价上涨{m}->{m+20}\n真寻酱共收了{str(tax(m))}金币的税"
         output = text_to_png(msg)
         await buyuser.finish(MessageSegment.image(output), at_sender=True)
     else:
         #被买走，钱给前主人
         auser = await UsersInfo.get_or_none(group_id=group_id,user_qq=at[0])
         if await BagUser.get_gold(user_id,group_id) < auser.body_price:
-            await buyuser.finish(f"你买不起！需要{auser.body_price}金币", at_sender=True)
+            await buyuser.finish(f"你买不起！需要{auser.body_price + tax(auser.body_price)}金币", at_sender=True)
             return
         if user_ := await GroupInfoUser.get_or_none(user_qq=user.muser_qq, group_id=group_id):
             user_name2 = user_.user_name
         m = await UsersInfo.add_user(user_id,group_id,at[0])
+        a_tax_s=m + tax(m)
+        a_tax_a=m - tax(m)
         if not m:
             await buyuser.finish("你已经是他的主人了！", at_sender=True)
             return
         else:
             if i := await UsersInfo.remove_user(user.muser_qq,group_id,at[0]):
-                await BagUser.add_gold(user.muser_qq,group_id,m)
-                await BagUser.spend_gold(user_id,group_id,m)
-                await BagUser.add_gold(at[0],group_id,m/10)
+                await BagUser.add_gold(user.muser_qq,group_id,a_tax_a)
+                await BagUser.spend_gold(user_id,group_id,a_tax_s)
+                await BagUser.add_gold(at[0],group_id,tax(m)/2)
                 a = await BagUser.get_gold(at[0],group_id)
                 u = await BagUser.get_gold(user_id,group_id)
                 u2 = await BagUser.get_gold(user.muser_qq,group_id)
-                msg=f"成功从{user_name2}那里购买了{user_name}！花费了{m}金币，还剩{u}金币\n{user_name2}获得了{m}金币，现在拥有{u2}金币\n{user_name}额外获得了{m/10}金币，现在拥有{a}金币，身价上涨{m}->{m+20}"
+                msg=f"成功从{user_name2}那里购买了{user_name}！花费了{a_tax_s}金币，还剩{u}金币\n{user_name2}获得了{a_tax_a}金币，现在拥有{u2}金币\n{user_name}额外获得了{tax(m)/2}金币，现在拥有{a}金币，身价上涨{m}->{m+20}\n真寻酱共收了{str(tax(m))}金币的税"
                 output = text_to_png(msg)
                 await buyuser.finish(MessageSegment.image(output), at_sender=True)
 
@@ -172,23 +178,24 @@ async def _(bot:Bot, event: GroupMessageEvent):
         gold = random.randint(10, 40)
         m = await UsersInfo.get_or_none(user_qq=user_id, group_id=group_id)
         gold=gold+ random.randint(m.body_price/10, m.body_price/5)
+        glod_after_tax=gold - tax(gold)
         await BagUser.add_gold(user_id,group_id,gold)
         u = await BagUser.get_gold(user_id,group_id)
         NICKNAME="【你】"
         msg=(
             random.choice(
                 [
-                    f"{NICKNAME}参加了网红主播的不要笑挑战。获得收入{str(gold)}金币",
-                    f"{NICKNAME}在闲鱼上卖东西，获得收入{str(gold)}金币",
-                    f"{NICKNAME}去在大街上发小传单，获得收入{str(gold)}金币",
-                    f"{NICKNAME}参加漫展，帮著名画师毛玉牛乳兜售新作，获得收入{str(gold)}金币",
-                    f"{NICKNAME}在美食街出售鸡你太美飞饼，虽然把饼甩飞了，但是围观群众纷纷购买鸡哥飞饼，获得收入{str(gold)}金币",
-                    f"{NICKNAME}偷渡到美国在中餐馆洗盘子，获得收入{str(gold)}金币",
-                    f"{NICKNAME}去黑煤窑挖煤，获得收入{str(gold)}金币",
-                    f"{NICKNAME}去横店当太君群演，被八路手撕了20次导演才说咔，获得收入{str(gold)}金币",
-                    f"{NICKNAME}去参加银趴服务别人，获得收入{str(gold)}金币",
-                    f"{NICKNAME}去拍摄小电影，获得收入{str(gold)}金币",
-                    f"{NICKNAME}去b站做审核员，看了十二小时旋转鸡块，获得收入{str(gold)}金币",
+                    f"{NICKNAME}参加了网红主播的不要笑挑战。获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                    f"{NICKNAME}在闲鱼上卖东西，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                    f"{NICKNAME}去在大街上发小传单，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                    f"{NICKNAME}参加漫展，帮著名画师毛玉牛乳兜售新作，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                    f"{NICKNAME}在美食街出售鸡你太美飞饼，虽然把饼甩飞了，但是围观群众纷纷购买鸡哥飞饼，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                    f"{NICKNAME}偷渡到美国在中餐馆洗盘子，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                    f"{NICKNAME}去黑煤窑挖煤，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                    f"{NICKNAME}去横店当太君群演，被八路手撕了20次导演才说咔，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                    f"{NICKNAME}去参加银趴服务别人，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                    f"{NICKNAME}去拍摄小电影，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                    f"{NICKNAME}去b站做审核员，看了十二小时旋转鸡块，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
                 ]
             )
         )
@@ -206,28 +213,28 @@ async def _(bot:Bot, event: GroupMessageEvent):
                 NICKNAME = f"【{user_.user_name}】"
             gold = random.randint(10, 40)
             gold=gold+ random.randint(p/10, p/5)
-            
+            glod_after_tax=gold - tax(gold)
             #10%概率没钱
             ran=random.randint(1,10)
             if ran != 6:
                 msg=(
                     random.choice(
                         [
-                            f"{NICKNAME}参加了网红主播的不要笑挑战。获得收入{str(gold)}金币",
-                            f"{NICKNAME}在闲鱼上卖东西，获得收入{str(gold)}金币",
-                            f"{NICKNAME}去在大街上发小传单，获得收入{str(gold)}金币",
-                            f"{NICKNAME}参加漫展，帮著名画师毛玉牛乳兜售新作，获得收入{str(gold)}金币",
-                            f"{NICKNAME}在美食街出售鸡你太美飞饼，虽然把饼甩飞了，但是围观群众纷纷购买鸡哥飞饼，获得收入{str(gold)}金币",
-                            f"{NICKNAME}偷渡到美国在中餐馆洗盘子，获得收入{str(gold)}金币",
-                            f"{NICKNAME}去黑煤窑挖煤，获得收入{str(gold)}金币",
-                            f"{NICKNAME}去横店当太君群演，被八路手撕了20次导演才说咔，获得收入{str(gold)}金币",
-                            f"{NICKNAME}去参加银趴服务别人，获得收入{str(gold)}金币",
-                            f"{NICKNAME}去拍摄小电影，获得收入{str(gold)}金币",
-                            f"{NICKNAME}去b站做审核员，看了十二小时旋转鸡块，获得收入{str(gold)}金币",
+                            f"{NICKNAME}参加了网红主播的不要笑挑战。获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                            f"{NICKNAME}在闲鱼上卖东西，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                            f"{NICKNAME}去在大街上发小传单，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                            f"{NICKNAME}参加漫展，帮著名画师毛玉牛乳兜售新作，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                            f"{NICKNAME}在美食街出售鸡你太美飞饼，虽然把饼甩飞了，但是围观群众纷纷购买鸡哥飞饼，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                            f"{NICKNAME}偷渡到美国在中餐馆洗盘子，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                            f"{NICKNAME}去黑煤窑挖煤，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                            f"{NICKNAME}去横店当太君群演，被八路手撕了20次导演才说咔，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                            f"{NICKNAME}去参加银趴服务别人，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                            f"{NICKNAME}去拍摄小电影，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
+                            f"{NICKNAME}去b站做审核员，看了十二小时旋转鸡块，获得收入{str(glod_after_tax)}金币,真寻酱收了{str(tax(gold))}金币的税",
                         ]
                     )
                 )
-                golds=golds+gold
+                golds=golds+glod_after_tax
             else:
                 msg=(
                     random.choice(
@@ -252,3 +259,37 @@ async def _(bot:Bot, event: GroupMessageEvent):
         msgs=msgs+f"你总共获取{golds}金币，当前共有{u}金币"
         output = text_to_png(msgs)
         await work.finish(MessageSegment.image(output), at_sender=True)
+
+# 市场税率查询
+tax_rate = on_command("市场税率", aliases={"查询税率"}, permission=GROUP, priority=5, block=True)
+
+@tax_rate.handle()
+async def _(bot: Bot, event: GroupMessageEvent):
+    tax_table = [
+        {"区段": "0-1000", "税率": "1%"},
+        {"区段": "1001-2000", "税率": "2.5%"},
+        {"区段": "2001-5000", "税率": "4%"},
+        {"区段": "5000以上", "税率": "6%"},
+    ]
+    msg = "### 市场税率\n\n"
+    msg += "| 区段 | 税率 |\n"
+    msg += "| --- | --- |\n"
+    for row in tax_table:
+        msg += f"| {row['区段']} | {row['税率']} |\n"
+    output = await md_to_pic(md=msg)
+    await tax_rate.finish(MessageSegment.image(output), at_sender=True)
+
+def tax(price):
+    tax_rate = [0.01, 0.025, 0.04, 0.06]
+    if 0 <= price <= 1000:
+        tax = price * tax_rate[0]
+        return tax
+    if 1000 < price <= 2000:
+        tax = (price - 1000) * tax_rate[1] + tax_rate[0] * 1000
+        return tax
+    if 2000 < price <= 5000:
+        tax = (price - 2000) * tax_rate[2] + tax_rate[0] * 1000 + tax_rate[1] * 1000
+        return tax
+    if price > 5000:
+        tax = (price - 5000) * tax_rate[3] + tax_rate[0] * 1000 + tax_rate[1] * 1000 + tax_rate[2] * 3000
+        return tax
